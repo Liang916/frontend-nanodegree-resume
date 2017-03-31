ae5220bbb3229cc6921c06a30498cffb72311a94
Yuxia <yuxialiang@hotmail.com>
2017-03-31 14:05:58 +0800

    Finish Flow Control If statement, put up skills at glance

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,79 +1,112 @@
-var bio = {
+var bio =
+{
     "name":"Yuxia Liang",
     "role":"Web Developer",
-    "contacts":[{
-        "mobile":"1234567890",
-        "email":"yuxialiang@hotmail.com",
-        "github":"Liang916",
-        "location":"Shaoxing"
-    }],
+    "contacts":
+        {
+            "mobile":"1234567890",
+            "email":"yuxialiang@hotmail.com",
+            "github":"Liang916",
+            "location":"Shaoxing"
+        },
     "skill":["HTML","CSS","JavaScript","Python"],
     "bioPic":"image/fry.jpg"
-
 };
 
-var education = {
-    "university":[
-    {
-        "name":"University of Warwick",
-        "city":"Coventry, UK",
-        "degree":"BSc",
-        "major":"Management",
-        "description":"Focused on management of information systems, digital innovation, e-commerce and social media"
-    },
-    {
-        "name":"London School of Economics",
-        "city":"London, UK",
-        "degree":"MSc",
-        "major":"Management of Information Systems and Digital Innovation",
-        "description":"Focused on operations and project management, customer service management, economics for business, globalisation studies, financial reporting"
-    }
+var education =
+{
+    "university":
+    [
+        {
+            "name":"University of Warwick",
+            "city":"Coventry, UK",
+            "degree":"BSc",
+            "major":"Management",
+            "description":"Focused on management of information systems, digital innovation, e-commerce and social media"
+        },
+        {
+            "name":"London School of Economics",
+            "city":"London, UK",
+            "degree":"MSc",
+            "major":"Management of Information Systems and Digital Innovation",
+            "description":"Focused on operations and project management, customer service management, economics for business, globalisation studies, financial reporting"
+        }
     ]
 };
 
-var project = {
-    "project1":{
+var project =
+{
+    "project1":
+    {
         "name":"Deloitte Bootcamp Project",
         "description":"Obtained distinction in the Deloitte Boot Camp Project in which the team developed a cloud based CRM plan for a music app"
-
     },
-    "project2":{
+    "project2":
+    {
         "name":"Global Integral Project",
         "description":"Analysed different country profiles"
     }
 };
 
-var work = {
-    "PartTime":[
-    {
-        "company":"Subway",
-        "position":"Marketing Trainee",
-        "time":"7/2015-9/2015",
-        "description":[
-            "Achieved high customer satisfaction due to excellent service",
-            "Increased revenue"
-            ]
-    }
+var work =
+{
+    "PartTime":
+    [
+        {
+            "company":"Subway",
+                "position":"Marketing Trainee",
+            "time":"7/2015-9/2015",
+            "description":[
+                "Achieved high customer satisfaction due to excellent service",
+                "Increased revenue"
+                ]
+        }
     ],
-    "Internship":[
-    {
-        "company":"Zhongyu Real Estate Ltd.",
-        "position":"Accountant",
-        "time":"7/2014-9/2014",
-        "description":[
-            "Organised and input everyday income and expenses",
-            "Prepared documents for loan application"
-            ]
-    },
-    {
-        "company":"Aiku Garment Factory",
-        "position":"Order Coordinator",
-        "time":"7/2013-8/2013",
-        "description":[
-            "Allocated orders from America and Africa",
-            "Scheduled meetings between the director and trading partners",
-            "Translated documents"
-            ]
-    }
+    "Internship":
+    [
+        {
+            "company":"Zhongyu Real Estate Ltd.",
+            "position":"Accountant",
+            "time":"7/2014-9/2014",
+            "description":
+                [
+                "Organised and input everyday income and expenses",
+                "Prepared documents for loan application"
+                ]
+        },
+        {
+            "company":"Aiku Garment Factory",
+            "position":"Order Coordinator",
+            "time":"7/2013-8/2013",
+            "description":
+                [
+                "Allocated orders from America and Africa",
+                "Scheduled meetings between the director and trading partners",
+                "Translated documents"
+                ]
+        }
     ]
 }
+
+if (bio.skill.length > 0){
+    $("#header").append(HTMLskillsStart);
+    var formattedSkill = HTMLskills.replace("%data%",bio.skill[0]);
+    $("#skills").append(formattedSkill);
+    formattedSkill = HTMLskills.replace("%data%",bio.skill[1]);
+    $("#skills").append(formattedSkill);
+    formattedSkill = HTMLskills.replace("%data%",bio.skill[2]);
+    $("#skills").append(formattedSkill);
+    formattedSkill = HTMLskills.replace("%data%",bio.skill[3]);
+    $("#skills").append(formattedSkill);
+}
+
+
+
+
+
+
+
+
+
+
+
