7295fbf7625bcb3b9967927ac115e45adc2d8e05
Yuxia <yuxialiang@hotmail.com>
2017-03-31 13:32:57 +0800

    Add Yuxia's own bio,education,work,project

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,79 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var bio = {
+    "name":"Yuxia Liang",
+    "role":"Web Developer",
+    "contacts":[{
+        "mobile":"1234567890",
+        "email":"yuxialiang@hotmail.com",
+        "github":"Liang916",
+        "location":"Shaoxing"
+    }],
+    "skill":["HTML","CSS","JavaScript","Python"],
+    "bioPic":"image/fry.jpg"
+
+};
+
+var education = {
+    "university":[
+    {
+        "name":"University of Warwick",
+        "city":"Coventry, UK",
+        "degree":"BSc",
+        "major":"Management",
+        "description":"Focused on management of information systems, digital innovation, e-commerce and social media"
+    },
+    {
+        "name":"London School of Economics",
+        "city":"London, UK",
+        "degree":"MSc",
+        "major":"Management of Information Systems and Digital Innovation",
+        "description":"Focused on operations and project management, customer service management, economics for business, globalisation studies, financial reporting"
+    }
+    ]
+};
+
+var project = {
+    "project1":{
+        "name":"Deloitte Bootcamp Project",
+        "description":"Obtained distinction in the Deloitte Boot Camp Project in which the team developed a cloud based CRM plan for a music app"
+
+    },
+    "project2":{
+        "name":"Global Integral Project",
+        "description":"Analysed different country profiles"
+    }
+};
+
+var work = {
+    "PartTime":[
+    {
+        "company":"Subway",
+        "position":"Marketing Trainee",
+        "time":"7/2015-9/2015",
+        "description":[
+            "Achieved high customer satisfaction due to excellent service",
+            "Increased revenue"
+            ]
+    }
+    ],
+    "Internship":[
+    {
+        "company":"Zhongyu Real Estate Ltd.",
+        "position":"Accountant",
+        "time":"7/2014-9/2014",
+        "description":[
+            "Organised and input everyday income and expenses",
+            "Prepared documents for loan application"
+            ]
+    },
+    {
+        "company":"Aiku Garment Factory",
+        "position":"Order Coordinator",
+        "time":"7/2013-8/2013",
+        "description":[
+            "Allocated orders from America and Africa",
+            "Scheduled meetings between the director and trading partners",
+            "Translated documents"
+            ]
+    }
+    ]
+}
